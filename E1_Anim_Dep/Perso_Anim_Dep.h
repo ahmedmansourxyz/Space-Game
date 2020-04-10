f960826bf92a763fc3fcfd6cb68ff1cea7cd57f9
Firas <61876437+Firas-Bha@users.noreply.github.com>
2020-04-10 23:57:55 +0200

    F_BHA

---
+++ E1_Anim_Dep/Perso_Anim_Dep.h
@@ -0,0 +1,58 @@
+
+#define Perso_Anim_Dep_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_mixer.h>
+
+typedef struct
+{
+	// pour l'affichage
+	SDL_Surface *image;
+	SDL_Rect source;
+	SDL_Rect dest;
+	// taille du sprite
+
+	int largeur;
+	int hauteur;
+
+	// direction dans laquelle se deplace le sprite
+	int direction;
+
+	// vitesse du sprite
+	int vitesse;
+	
+	// pour la gestion des animations
+	
+	// si le sprite est anime
+	int anim;
+	
+	// direction dans laquelle est orienté le sprite
+	int orientation;
+
+	// animation courante du sprite
+	int anim_courante;
+
+	// le temps que dure une animation
+	int temps_anim;
+
+	// le temps de l'animation courante
+	int temps_anim_courante;
+
+	// nombre d'animations du sprite
+	int total_anims;
+
+}Sprite;
+
+
+
+
+
+SDL_Surface *loadBmp(const char *fichier);
+int Inistialiser_Perso( Sprite *sprite, const char *image );
+void Supprimer_Perso( Sprite *sprite );
+void Direction_Perso( Sprite *sprite, int direction );
+void miseAJourSprite( Sprite *sprite );
+void Afficher_Perso( Sprite *sprite, SDL_Surface *destination );
---
+++ E1_Anim_Dep/main.c
@@ -0,0 +1,141 @@
+
+#include <time.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include "Perso_Anim_Dep.h"
+
+#define HAUT	0
+#define DROITE	1
+#define BAS	2
+#define GAUCHE	3
+
+#define AUCUNE_DIRECTION	0
+#define DIRECTION_HAUT		1
+#define DIRECTION_DROITE	2
+#define DIRECTION_BAS		4
+#define DIRECTION_GAUCHE	8
+
+#define LARGEUR_ECRAN 640
+#define HAUTEUR_ECRAN 480
+
+int main()
+{
+
+	SDL_Surface *ecran;
+	SDL_Event event;
+	int continuer = 0;
+	Uint32 temps_courant, prochain_rendu = 0;
+	Sprite Perso;
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0)
+	{
+		fprintf(stderr, "Echec d'initialisation de SDL.\n");
+		return 1;
+	}
+	printf("Bonjour le monde, SDL est initialisé avec succès.\n");
+	ecran = SDL_SetVideoMode(640, 480, 16, SDL_FULLSCREEN);
+	if ( ecran == NULL )
+	{
+		fprintf(stderr, "Echec de changement du mode video : %s.\n", SDL_GetError());
+		return 1;
+	}
+	if ( !Inistialiser_Perso( &Perso, "perso.bmp" ) )
+	{
+		SDL_Quit();
+		return 1;
+	}
+	// on place le sprite que l'on controle au milieu
+	Perso.dest.x = 304;
+	Perso.dest.y = 260;
+	Direction_Perso( &Perso, DIRECTION_BAS );
+	Direction_Perso( &Perso, AUCUNE_DIRECTION );
+
+	while (!continuer)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_ESCAPE:
+					continuer = 1;
+					break;
+				case SDLK_UP:
+					Direction_Perso( &Perso, DIRECTION_HAUT );
+					break;
+				
+				case SDLK_RIGHT:
+					Direction_Perso( &Perso, DIRECTION_DROITE );
+					break;
+				
+				case SDLK_DOWN:
+					Direction_Perso( &Perso, DIRECTION_BAS );
+					break;
+				
+				case SDLK_LEFT:
+					Direction_Perso( &Perso, DIRECTION_GAUCHE );
+					break;
+				
+				
+				}
+				break;
+			
+			case SDL_KEYUP:
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_UP:
+					if (Perso.direction == DIRECTION_HAUT)
+						Direction_Perso( &Perso, AUCUNE_DIRECTION );
+					break;
+				
+				case SDLK_RIGHT:
+					if (Perso.direction == DIRECTION_DROITE)
+						Direction_Perso( &Perso, AUCUNE_DIRECTION );
+					break;
+				
+				case SDLK_DOWN:
+					if (Perso.direction == DIRECTION_BAS)
+						Direction_Perso( &Perso, AUCUNE_DIRECTION );
+					break;
+				
+				case SDLK_LEFT:
+					if (Perso.direction == DIRECTION_GAUCHE)
+						Direction_Perso( &Perso, AUCUNE_DIRECTION );
+					break;
+				}
+				break;
+
+			case SDL_QUIT:
+				continuer = 1;
+				break;
+			
+			default:
+				;
+			}
+		}
+		
+		/* On recupère le temps écoulé en mili-secondes */
+		temps_courant = SDL_GetTicks();
+		
+		/* On regarde s'il est temps d'effectuer le prochain rendu */
+		if (temps_courant > prochain_rendu)
+		{
+			/* un rendu toutes les 20 milli-secondes = 50 images par secondes */
+			prochain_rendu = temps_courant + 20;
+			miseAJourSprite( &Perso );
+			SDL_FillRect(ecran, NULL, 0);
+			Afficher_Perso( &Perso, ecran );	
+			/* On met à jour de la zone d'affichage de la fenetre */
+			SDL_UpdateRect(ecran, 0, 0, 0, 0);
+		}
+		
+	}
+
+	
+	Supprimer_Perso( &Perso );
+	SDL_Quit();
+	return 0;
+}
